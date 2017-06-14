bfbb187a1dcdf3bed0e2cdcd02ccb13b45e23c2b
Rozario708 <rozario708@outlook.com>
2017-06-14 17:57:16 +0530

    Create StrInt.java

---
+++ StrInt.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class StrInt
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int n=Integer.parseInt(s);
+        System.out.println(n);
+    }
+}
+
