96e836b5bd8bfa89a7361dd192ff70055030b533
Rozario708 <rozario708@outlook.com>
2017-06-14 15:34:12 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class factorial
+{
+    public static void main(String args[])
+    {
+        int fact=1;
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
