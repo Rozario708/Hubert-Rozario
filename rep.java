91613109bdce9f739eba803ebeabd3fc17c84b5c
Rozario708 <rozario708@outlook.com>
2017-06-22 15:02:16 +0530

    Create rep.java

---
+++ rep.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class rep
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int flag=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    flag=1;
+                    System.out.println(a[i]);
+                }
+            }
+        }
+    }
+}
+
+
+
