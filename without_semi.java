1d30eaa9eb7b9511a7e15863db6e43aee86ce39d
Rozario708 <rozario708@outlook.com>
2017-09-26 10:01:50 +0530

    Create without_semi.java

---
+++ without_semi.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class without_semi
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        if(System.out.printf("hello world")==null)
+        {
+            
+        }
+    }
+}
