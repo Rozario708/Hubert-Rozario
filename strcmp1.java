ea6a1b237895beb526dfc76aae47504207fbb47a
Rozario708 <rozario708@outlook.com>
2017-09-26 10:49:32 +0530

    Rename strcmp.java to strcmp1.java

---
+++ strcmp1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class strcmp
+{
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       {
+           String s1=sc.nextLine();
+           String s2=sc.nextLine();
+           int count=0;
+           if(s1.length()!=s2.length())
+           {
+               System.out.println("false");
+           }
+           else
+           {
+               for(int i=0;i<s1.length();i++)
+               {
+                   if(s1.charAt(i)==s2.charAt(i))
+                   {
+                       count++;
+                   }
+               }
+               if(count==s2.length())
+               {
+                   System.out.println("true");
+               }
+               else
+               {
+                   System.out.println("false");
+               }
+           }
+       }
+    }
+}
